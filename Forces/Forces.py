A possible bug in Normal-force calculations or numerical solver, WIP

Forces/Forces.py
@@ -17,32 +17,33 @@ h = R*T0/(m*g)  # Height constant of Air ~ 1e4 [m]
 
 # Forces
 
-def SAMdrag(rocket, position, linearSpeed):
+def SAMdrag(rocket, position, linearVelocity):
     """
         Assumptions:- AoA ~ 0
                     - Quadratic drag F ~ -kv^2
     :param rocket: [rocket class] The rocket object
     :param position: [np.array] The position vector in world coordinates
-    :param linearVelocity: [np.array] The current linear velocity in world coord.
-    :return: [float]  Magnitude of drag force in the world frame
+    :param linearVelocity: [np.array] The current rocket velocity in world coord. (with wind)
+    :return: [np.array] drag force in the world frame
     """
     z = abs(position[2])  # Vertical position of rocket
     Cd = rocket.getCd()
     Aref = np.pi*(rocket.getBody().getDiameter()/2)**2
     k = 1/2*rho0*Aref*Cd*np.exp(-z/h)
 
-    return k*linearSpeed**2
+    return -k*np.linalg.norm(linearVelocity)*linearVelocity
 
-def SAMlift(rocket, position, linearSpeed, AoA):
+def SAMlift(rocket, position, linearVelocity, AoA):
     """
     :param rocket: [rocket class] The rocket object
     :param position: [np.array] The position vector in world coordinates
-    :param linearVelocity: [np.array] The current linear velocity in world coord.
-    :return: [float] Magnitude of lift force in the world frame
+    :param linearVelocity: [np.array] The current rocket velocity in world coord. (with wind)
+    :return: [np.array] Lift force in the body frame
     """
     z = abs(position[2])  # Vertical position of rocket
     Cn = rocket.getCn(AoA)
     Aref = np.pi*(rocket.getBody().getDiameter()/2)**2
     k = 1/2*rho0*Aref*Cn*np.exp(-z/h)
+    speed = np.linalg.norm(linearVelocity)
 
-    return k*linearSpeed**2
+    return k*speed**2

Rocket/Rocket1.py
@@ -660,7 +660,7 @@ class RocketSimple:
         #Nose, Body and Fins Cn
         CNnose = self.__CNnose*np.sinc(AoA/np.pi) # np.sinc(x) = sin(pi*x)/(pi*x)
         CNfin = self.__CNfin*np.sinc(AoA/np.pi)
-        CNbody = self.__CNbody*np.sin(AoA)**2
+        CNbody = self.__CNbody*(np.sinc(AoA/np.pi)**2)*AoA
         CNrocket = CNnose + CNbody + CNfin
         COP0 = (CNnose*self.__Xcp_nose + CNbody*self.__Xcp_body + CNfin*self.__Xcp_fin)/CNrocket
         return np.array([COP0, 0, 0])
@@ -670,15 +670,15 @@ class RocketSimple:
         COP = self.getCOP(AoA)[0]
         return COM - COP
 
-    def getAeroForces(self, AoA, position, speed):
+    def getAeroForces(self, AoA, position, velocity):
         """
-        :param speed: [float] speed of air relative to rocket [m/s]
+        :param velocity: [np.array] velocity of rocket (with wind) relative to world [m/s]
         :param AoA: [float] the angle of attack [rad]
 
         :return: [np.array] ([drag, lift]) on rocket attacking in COP [N]
         """
-        drag = Forces.SAMdrag(self, position, speed)
-        lift = Forces.SAMlift(self, position, speed, AoA)
+        drag = Forces.SAMdrag(self, position, velocity)
+        lift = Forces.SAMlift(self, position, velocity, AoA)
         return np.array([drag, lift])
 
     def getMomentAboutCOM(self, position, velocity, AoA):
@@ -697,7 +697,7 @@ class RocketSimple:
         #Nose, Body and Fins Cn
         CNnose = self.__CNnose*np.sinc(AoA/np.pi)
         CNfin = self.__CNfin*np.sinc(AoA/np.pi)
-        CNbody = self.__CNbody*np.sin(AoA)**2
+        CNbody = self.__CNbody*(np.sinc(AoA/np.pi)**2)*AoA
         # FINAL ROCKET COP
         CNrocket = CNnose + CNbody + CNfin
         return CNrocket

Tests/test_trajectory_module.py
@@ -20,9 +20,9 @@ def test_trajectory_module():
     init_file = 'V13_data.dot'
     path = 'V13/'
     rocket2 = Rocket.from_file_with_AoAspeed(init_file, sample_file, path)
-    initialInclination = 1.0/180.0*np.pi
+    initialInclination = 10/180.0*np.pi
     launchRampLength = 2.5*rocket1.getLength()
-    timeStep = 0.005
+    timeStep = 0.003
     simulationTime= 15
     (t, position, euler, linearVelocity, angularVelocity, AoA, thrust, gravity, drag, lift) \
     = Trajectory.calculateTrajectory(rocket1, initialInclination, launchRampLength, timeStep, simulationTime)

Trajectory/Trajectory.py
@@ -79,21 +79,22 @@ def equationsMotion(rocket, x, t, launchRampLength, initialDirection):
     gravity = RotationInertial2Body @ np.array([0, 0, rocket.getMass(t)*g])
     # aerodynamic forces
     windVelocity = np.array([0, 0, 0])
-    airVelocity = linearVelocity - windVelocity
+    # Add wind to current rocket velocity to get total air velocity
+    airVelocity = linearVelocity + windVelocity
     airSpeed = np.linalg.norm(airVelocity)
     xAxisBody = RotationBody2Inertial[:,0]
     dirWindVelocity = (airVelocity/(np.linalg.norm(airVelocity) + epsilon))
     AoA = np.arccos(np.dot(dirWindVelocity, xAxisBody))
     dirDragBody = RotationInertial2Body @ dirWindVelocity
-    aeroForces = rocket.getAeroForces(AoA, position, airSpeed)
-    drag = -aeroForces[0]*dirDragBody
+    #TODO Problem with drag and lift directions
     projectedDragBody = np.array([0, dirDragBody[1], dirDragBody[2]])
     dirProjectedDragBody = projectedDragBody/(np.linalg.norm(projectedDragBody) + epsilon)
     dirLiftBody = np.sin(AoA)*np.array([1, 0, 0]) + np.cos(AoA)*dirProjectedDragBody
-    lift = -aeroForces[1]*dirLiftBody
+    aeroForces = rocket.getAeroForces(AoA, position, airVelocity)
+    drag = RotationInertial2Body @ aeroForces[0]
+    lift = -aeroForces[1]*dirLiftBody*0
     # inertia matrix and coriolis matrix for equations of motion
     # seen from origin of body frame, not from center of mass (See Fossen)
-    print(lift)
     H = Kinematics.TransformationMatrix(rocket.getCOM(t))
     m = rocket.getMass(t)
     I = rocket.getInertiaMatrix(t)