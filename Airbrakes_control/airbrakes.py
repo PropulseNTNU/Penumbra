71f0bb212f83b6810efe0689e11dbd4b6249b677
havard <hloevi@live.no>
2019-03-19 12:32:46 +0100

    Made kalman test on test data

--- Airbrakes_control/airbrakes.py
+++ Airbrakes_control/airbrakes.py
@@ -2,6 +2,9 @@ import control
 import kalman_filter
 import interpolation
 import math
+from numpy import genfromtxt
+
+
 dt=0.03
 ki=0.01
 kp=1
@@ -23,3 +26,23 @@ def airbrakes_main(height, acc, dt):
     else:
         u*=((math.pi)/180)
     return control.calculate_area(u), estimated_h, estimated_v, error
+
+def test_kalman():
+    data = genfromtxt('data/flight1.csv', delimiter=',')
+    estimates_h = []
+    estimates_v = []
+    prev_time = data[0][0] - 30 #just to have a dt at the start
+    for row in data:
+        print(row[0])
+        dt = (row[0] - prev_time) / 1000
+        est_h, est_v = kalman_filter.kalman(row[4], row[21], dt)
+        estimates_h.append(est_h.item(0))
+        estimates_v.append(est_v.item(0))
+        prev_time = row[0]
+
+    print("Height: ")
+    print(estimates_h)
+    print("Velocity: ")
+    print(estimates_v)
+
+test_kalman()
