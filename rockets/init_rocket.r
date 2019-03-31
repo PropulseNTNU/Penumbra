415f78073b9c8b759e0a872e799e1de1bcdbf4ed
hermandrage <hermandr@stud.ntnu.no>
2019-03-31 16:08:53 +0200

    added rockets and Airbrakes_control source code

---
+++ rockets/init_rocket.r
@@ -0,0 +1,13 @@
+Parts:
+nose = r2/nose2.p
+body = r2/body2.p
+fin = r2/fin2.p
+motor = motors/TR_motor.dot
+payload = r2/payload.p
+
+Number of fins:
+number_of_fins = 3
+
+Part placement [meters]:
+fin_placement = -840e-3
+payload_placement = -100e-3
---
+++ rockets/r2/fin2.p
@@ -0,0 +1,6 @@
+root_chord = 100e-3
+root_angle = 30.5
+tip_chord = 60e-3
+semi_chord = 85e-3
+thickness = 1e-3
+density = 680
