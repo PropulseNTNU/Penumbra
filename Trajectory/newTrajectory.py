0e182c76e9d79fc11016b7faae8146585841dab2
Tobias <t.winter2102@gmail.com>
2019-04-07 14:54:16 +0200

    New trajectory structure comenced

---
+++ Trajectory/newTrajectory.py
@@ -0,0 +1,23 @@
+
+class Trajectory:
+    def __init__(rocket, initialInclination, rampLength, dragDeviation = 0,\
+    windObj = Wind.nullWind()):
+        self.dt = 0.03 # temp
+        # Initial state
+
+    def run(simTime = 30, trim = False):
+        pass
+
+    def flush():
+        # flush data
+        pass
+
+    def plot(plot1 = False, plot2 = False, plot3 = False):
+        pass
+
+    def __str__():
+        pass
+
+    # get functions here
+
+    # set functions here
